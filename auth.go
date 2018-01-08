600f965668459aed0e1eed86e8741ae8b8453fd2
後閑諒一 <nargok@gmail.com>
2018-01-08 16:45:05 +0900

    Create loginHandler

--- auth.go
+++ auth.go
@@ -1,10 +1,16 @@
 package main
+
 import (
   "net/http"
+  "fmt"
+  "strings"
+  "log"
 )
+
 type authHandler struct {
   next http.Handler
 }
+
 func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
   if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
     // 未認証
@@ -18,6 +24,25 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     h.next.ServeHTTP(w, r)
   }
 }
+
 func MustAuth(handler http.Handler) http.Handler {
   return &authHandler{next: handler}
+}
+
+// loginHandlerはサーボパーティーへのログインの処理を受け持ちます
+// パスの形式: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+
+  // URLを/ごとに分割してactionとproviderを取り出す
+  segs := strings.Split(r.URL.Path, "/")
+  action := segs[2]
+  provider := segs[3]
+
+  switch action {
+  case "login":
+    log.Println("TODO: ログイン処理", provider)
+  default:
+    w.WriteHeader(http.StatusNotFound)
+    fmt.Fprintf(w, "アクション%sには非対応です", action)
+  }
 }
